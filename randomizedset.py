9431fb793fadf5bf6e5c53c7a60b97c6a3daf7f3
koziol <lukasz.koziol@dell.com>
2024-10-01 11:44:13 +0200

    leetcode

--- randomizedset.py
+++ randomizedset.py
@@ -24,15 +24,16 @@ class RandomizedSet:
         self.indexes.pop(val)
         if len(self.values) > 1:
             last_ix = len(self.values) - 1
-            # move back last value to the removed value position
-            last_value = self.values[last_ix]
-            self.values[removed_ix] = last_value # !!!
-            # remove last value which is now in 2 places
-            self.values.pop()
-            # update map with new last-element index that was moved back
-            self.indexes[last_value] = removed_ix
-        else:
-            self.values.pop()
+            if removed_ix < last_ix:
+                # move back last value to the removed value position
+                last_value = self.values[last_ix]
+                self.values[removed_ix] = last_value  # !!!
+
+                # update map with new last-element index that was moved back
+                self.indexes[last_value] = removed_ix
+
+        # remove last value which can now be in 2 places (if len>1)
+        self.values.pop()
 
         return True
 
@@ -41,13 +42,11 @@ class RandomizedSet:
 
 
 s = RandomizedSet()
-print(s.insert(0))
-print(s.remove(0))
-print(s.insert(-1))
-print(s.remove(0))
+print(s.insert(1))
+print(s.insert(7))
+print(s.remove(1))
 
 print(s.getRandom())
 print(s.getRandom())
 print(s.getRandom())
 print(s.getRandom())
-
