2c6d28f5de3401875b202c1ae3e244af904b2d40
koziol <lukasz.koziol@dell.com>
2024-12-04 15:55:22 +0100

    leetcode

--- reverseWords.py
+++ reverseWords.py
@@ -9,6 +9,13 @@ class Solution:
             start += 1
             end -= 1
 
+    def copy_subarray(self, s: List, start: int, end: int, write_index: int):
+        while start < end:
+            s[write_index] = s[start]
+            start += 1
+            write_index += 1
+        return write_index
+
     def find_next_word_boundaries(self, s: List, start_ix: int) -> (int, int):
         while start_ix < len(s) and s[start_ix] == ' ':
             start_ix += 1
@@ -28,18 +35,34 @@ class Solution:
         while end_ix >= 0 and t[end_ix] == ' ':
             end_ix -= 1
 
+        # trim the list
         t = t[start_ix: end_ix + 1]
+
         # reverse the whole string
         self.reverse_subarray(t, 0, len(t) - 1)
-        # starting at index 0 find next word boundary, reverse it and continue until end of string
+
+        # starting at index 0 find next word boundary, reverse it and move it to write_index, move write_index past end_ix
+        # and continue until end of string
+        write_index = 0
         start_ix = 0
-        while start_ix < len(t):
+        while start_ix < len(t) and write_index < len(t):
             start_ix, end_ix = self.find_next_word_boundaries(t, start_ix)
             self.reverse_subarray(t, start_ix, end_ix - 1)
+            # move the word to the write_index
+            write_index = self.copy_subarray(t, start_ix, end_ix, write_index)
+            # move write_index past end_ix
             start_ix = end_ix
+
+            if write_index < len(t):
+                t[write_index] = ' '
+            write_index += 1
+
+        t = t[0:write_index - 1]
+
         return ''.join(t)
 
 
 st = "  the sky is blue  "
+st = "a good   example"
 s = Solution()
 print(s.reverseWords(st))
