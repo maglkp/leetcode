fa3f62ff9d6cdf15ee9b264a560fa5e8247f532a
koziol <lukasz.koziol@dell.com>
2025-01-20 16:46:54 +0100

    leetcode

--- calculate.py
+++ calculate.py
@@ -7,7 +7,7 @@ class Solution:
 
     def calculate(self, s: str) -> int:
         s = re.split(r'(\d+)|(\s+)|(\+)|(-)|(\()|(\))', s)
-        s = [i for i in s if i and i != ' ']
+        s = [i for i in s if i and not i.isspace()]
 
         ix = 0
         stack = deque()
@@ -21,6 +21,22 @@ class Solution:
                 value, ix_inc = self.eval(s[ix + 1:])
                 stack.append(value)
                 ix += ix_inc + 1
+            elif s[ix] == '-':
+                if not stack:
+                    left = 0
+                else:
+                    left = int(stack.pop())
+
+                if s[ix + 1].isdigit():
+                    right = int(s[ix + 1])
+                    stack.append(left - right)
+                    ix += 2
+                elif s[ix + 1] == '(':
+                    right, ix_inc = self.eval(s[ix + 2:])
+                    stack.append(left - int(right))
+                    ix += ix_inc + 2
+                else:
+                    raise ValueError("Expected digit or ) after + or -")
             elif s[ix] == '+':
                 left = int(stack.pop())
                 if s[ix + 1].isdigit():
@@ -29,12 +45,12 @@ class Solution:
                     ix += 2
                 elif s[ix + 1] == '(':
                     right, ix_inc = self.eval(s[ix + 2:])
-                    stack.append(left + right)
-                    ix += ix_inc + 1
+                    stack.append(left + int(right))
+                    ix += ix_inc + 2
                 else:
                     raise ValueError("Expected digit or ) after + or -")
 
-        return stack.pop()
+        return int(stack.pop())
 
     def eval(self, s: List[str]) -> (int, int):
         ix = 0
@@ -49,6 +65,22 @@ class Solution:
                 value, ix_inc = self.eval(s[ix + 1:])
                 stack.append(value)
                 ix += ix_inc + 1
+            elif s[ix] == '-':
+                if not stack:
+                    left = 0
+                else:
+                    left = int(stack.pop())
+
+                if s[ix + 1].isdigit():
+                    right = int(s[ix + 1])
+                    stack.append(left - right)
+                    ix += 2
+                elif s[ix + 1] == '(':
+                    right, ix_inc = self.eval(s[ix + 2:])
+                    stack.append(left - int(right))
+                    ix += ix_inc + 2
+                else:
+                    raise ValueError("Expected digit or ) after + or -")
             elif s[ix] == '+':
                 left = int(stack.pop())
                 if s[ix + 1].isdigit():
@@ -57,8 +89,8 @@ class Solution:
                     ix += 2
                 elif s[ix + 1] == '(':
                     right, ix_inc = self.eval(s[ix + 2:])
-                    stack.append(left + right)
-                    ix += ix_inc + 1
+                    stack.append(left + int(right))
+                    ix += ix_inc + 2
                 else:
                     raise ValueError("Expected digit or ) after + or -")
 
@@ -67,5 +99,24 @@ class Solution:
 
 st = "-1 + 1 - 2 - (2+3)"
 st = "1 + (1+2)"
+st = "(((((4)))))"
+st = "1-1"
+st = "(1+(4+5+2)-3)+(6+8)"
+st = "0"
+st = "  30"
+st = "(7)-(0)+(4)"
+st = "2-4-(8+2-6+(8+4-(1)+8-10))"
+st = "(8+4-(1)+8-10)" # 9 ok
+st = "(8+2-6+(8+4-(1)+8-10))" # 13 k
+st = "(-(13))"
+st = "1-(5)"
+st = "(1+(4+5+2)-3)+(6+8)" # 23
+st = "(9)+(14)" # we start with (), have to increase by 1 only
+st = "-(8+2-6+(8+4-(1)+8-10))" # -13
+st = "-(4+(12-(1)-2))" # -13 we start with operator, have to increase by +2
+st = "(1+(4+5+2)-3)+(6+8)"
+st = "2-4-(8+2-6+(8+4-(1)+8-10))" # -15
+#st = "-(8+2-6+(8+4-(1)+8-10))" # -13
+st = "(9)+(14)"
 s = Solution()
 print(s.calculate(st))
