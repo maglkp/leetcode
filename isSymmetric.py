78f3c3845ab48b1e824c6c1574d0c61095f383a2
koziol <lukasz.koziol@dell.com>
2025-01-23 16:43:06 +0100

    leetcode

--- isSymmetric.py
+++ isSymmetric.py
@@ -6,5 +6,17 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        return self.isMirrorImage(root.left, root.right)
+
+    def isMirrorImage(self, node_on_left_side, node_on_right_side):
+        if node_on_left_side is None and node_on_right_side is None:
+            return True
+        if node_on_left_side is None or node_on_right_side is None:
+            return False
+        if node_on_left_side.val != node_on_right_side.val:
+            return False
+        return (self.isMirrorImage(node_on_left_side.left, node_on_right_side.right) and
+                self.isMirrorImage(node_on_left_side.right, node_on_right_side.left))
