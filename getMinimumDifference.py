676b667f49835bc2579ff57a390b1167ff2a588b
koziol <lukasz.koziol@dell.com>
2025-03-17 13:56:07 +0100

    leetcode

--- getMinimumDifference.py
+++ getMinimumDifference.py
@@ -11,21 +11,42 @@ class TreeNode:
 
 class Solution:
     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
-        return self.dfs(root)
-
-    def dfs(self, node) -> Optional[int]:
         limit = 10 ** 5 + 1
-        if not node:
-            return None
+        prev_min_val = [None, limit]
 
-        l = self.dfs(node.left) or limit
-        r = self.dfs(node.right) or limit
+        def dfs(node):
+            if not node:
+                return
 
-        if node.left is not None and node.right is not None:
-            return min(abs(node.left.val - node.val), abs(node.right.val - node.val), l, r)
-        elif node.right is not None:
-            return min(abs(node.right.val - node.val), r)
-        elif node.left is not None:
-            return min(abs(node.left.val - node.val), l)
-        else:
-            return None
+            dfs(node.left)
+            if prev_min_val[0] is not None:
+                prev_min_val[1] = min(abs(node.val - prev_min_val[0]), prev_min_val[1])
+
+            prev_min_val[0] = node.val
+            dfs(node.right)
+
+        dfs(root)
+
+        return prev_min_val[1]
+
+
+tree = TreeNode(val=100000, left=TreeNode(val=0))
+s = Solution()
+print(s.getMinimumDifference(tree))
+
+# def dfs(self, node) -> Optional[int]:
+#     limit = 10 ** 5 + 1
+#     if not node:
+#         return None
+#
+#     l = self.dfs(node.left) or limit
+#     r = self.dfs(node.right) or limit
+#
+#     if node.left is not None and node.right is not None:
+#         return min(abs(node.left.val - node.val), abs(node.right.val - node.val), l, r)
+#     elif node.right is not None:
+#         return min(abs(node.right.val - node.val), r)
+#     elif node.left is not None:
+#         return min(abs(node.left.val - node.val), l)
+#     else:
+#         return None
