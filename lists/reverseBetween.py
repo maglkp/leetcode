0a700b3c481a3212bf7b32e5c378a1b9682286b7
koziol <lukasz.koziol@dell.com>
2025-04-30 14:48:07 +0200

    leetcode

--- lists/reverseBetween.py
+++ lists/reverseBetween.py
@@ -8,19 +8,41 @@ class ListNode:
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
 
-        current = head
+        if left == right:
+            return head
 
-        # advance head left-1 times
+        dummy = ListNode(0)
+        dummy.next = head
+        current = dummy
+
+        #current = head
+
+        # advance head left - 1 times
         for _ in range(left - 1):
             current = current.next
 
+        # one before left
+        one_before_left = current
+        left_node = current.next
+
+        # remove the link just before the start of the sublist to be reversed
         nxt = current.next
         current.next = None
 
-        for _ in range(right):
+        # set the initial pointers
+        current = nxt
+        nxt = nxt.next
+
+        for _ in range(right - left):
             new_nxt = nxt.next
             nxt.next = current
             current = nxt
             nxt = new_nxt
 
-        return head
+        one_past_right = new_nxt
+
+        # connect reversed sublist, current being last element of the reversed sublist
+        one_before_left.next = current
+        left_node.next = one_past_right
+
+        return dummy.next
