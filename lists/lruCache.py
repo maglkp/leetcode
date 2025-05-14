e22099b39130542db04b40ff473c532909af96b7
koziol <lukasz.koziol@dell.com>
2025-05-14 10:28:00 +0200

    leetcode

--- lists/lruCache.py
+++ lists/lruCache.py
@@ -32,13 +32,16 @@ class LRUCache:
             #if entry is not self.leastRecentlyUsed:
             # or
             if entry.next is not None:
+                # it was in the middle
                 entry.prev.next = entry.next
                 entry.next.prev = entry.prev
                 self.mostRecentlyUsed.prev = entry
                 entry.next = self.mostRecentlyUsed
                 self.mostRecentlyUsed = entry
             else:
+                # it was the least recently used
                 entry.prev.next = None
+                self.leastRecentlyUsed = entry.prev
                 entry.prev = None
                 entry.next = self.mostRecentlyUsed
                 self.mostRecentlyUsed.prev = entry
@@ -50,10 +53,12 @@ class LRUCache:
 
         if key in self.cache:
             entry = self.cache[key]
+            entry.value = value
             if entry != self.mostRecentlyUsed:
                 # or
                 # if entry.prev is not None:
                 if entry is not self.leastRecentlyUsed:
+                    # is in the middle
                     # or
                     # if entry.next is not None:
                     entry.prev.next = entry.next
@@ -62,19 +67,24 @@ class LRUCache:
                     entry.next = self.mostRecentlyUsed
                     self.mostRecentlyUsed = entry
                 else:
-                    entry.prev.next = None
+                    # is the least recently used
+                    self.leastRecentlyUsed = entry.prev
+                    self.leastRecentlyUsed.next = None
                     entry.prev = None
                     entry.next = self.mostRecentlyUsed
                     self.mostRecentlyUsed.prev = entry
                     self.mostRecentlyUsed = entry
-            entry.value = value
         else:
             if len(self.cache) == self.capacity:
-                one_but_least = self.leastRecentlyUsed.prev
-                if one_but_least:
-                   one_but_least.next = None
                 del self.cache[self.leastRecentlyUsed.key]
-                self.leastRecentlyUsed = one_but_least
+
+                if self.leastRecentlyUsed != self.mostRecentlyUsed:
+                    one_but_least = self.leastRecentlyUsed.prev
+                    one_but_least.next = None
+                    self.leastRecentlyUsed = one_but_least
+                else:
+                    self.mostRecentlyUsed = None
+                    self.leastRecentlyUsed = None
 
             entry = Node(key, value)
             self.cache[key] = entry
@@ -87,17 +97,40 @@ class LRUCache:
                 self.mostRecentlyUsed = entry
 
 capacity = 2
-
 obj = LRUCache(capacity)
-obj.put(1, 1)
-obj.put(2, 2)
-print(obj.get(1))
-obj.put(3, 3)
+# obj.put(1, 1)
+# obj.put(2, 2)
+# print(obj.get(1))
+# obj.put(3, 3)
+# print(obj.get(2))
+# obj.put(4, 4)
+# print(obj.get(1))
+# print(obj.get(3))
+# print(obj.get(4))
+
+
+
+# obj.put(2, 1)
+# obj.put(1, 1)
+# obj.put(2, 3)
+# obj.put(4, 1)
+#
+# print(obj.get(1))
+# print(obj.get(2))
+
+
+#["LRUCache","get","get","put","get","put","put","put","put","get","put"]
+#[[1],[6],[8],[12,1],[2],[15,11],[5,2],[1,15],[4,2],[4],[15,15]]
+
+capacity = 1
+obj = LRUCache(capacity)
+print(obj.get(6))
+print(obj.get(8))
+print(obj.put(12, 1))
 print(obj.get(2))
-obj.put(4, 4)
-#print(obj.get(1))
-#print(obj.get(3))
-#print(obj.get(4))
-
-
-
+print(obj.put(15, 11))
+print(obj.put(5, 2))
+print(obj.put(1, 15))
+print(obj.put(4, 2))
+print(obj.get(4))
+print(obj.put(15, 15))
